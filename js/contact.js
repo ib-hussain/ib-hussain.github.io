194662323f6457c8716a2a79e106655fa942dcac
ib-hussain <ibrahimbeaconarion@gmail.com>
2025-06-15 01:58:58 +0500

    initial site made

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,6 @@
-// something
+document.querySelector(".form-fill").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank You!");
+  // submittion.href = "index.html";
+  this.reset();
+});
